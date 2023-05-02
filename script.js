6bf9df34d0088d3a433a094b6978c4cfcb9615a2
Ola <hollabodey.kilaso@gmail.com>
2023-05-02 16:43:03 +0100

    Made changes to the html and css files

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ let container = document.querySelector('.container');
 
 let GreenMode = document.getElementById('green');
 
+let Normal = document.getElementById('normal');
+
 
 DarkMode.addEventListener('click', darkMode);
-GreenMode.addEventListener('click', greenMode); 
+GreenMode.addEventListener('click', greenMode);
+Normal.addEventListener('click', normalMode); 
+
+function normalMode(e){
+    e.preventDefault();
+    container.classList.remove('darkMode');
+    container.classList.remove('green');
+}
 
 
 function darkMode(e){
     e.preventDefault();
-    container.classList.toggle('darkMode');
+    container.classList.remove('green');
+    container.classList.add('darkMode');
 
 }
 
 function greenMode(e){
     e.preventDefault();
-    container.classList.toggle('green');
+    container.classList.add('green');
 }
+
