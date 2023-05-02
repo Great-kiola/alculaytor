b9de5cafc6fcaf02d6c3d67ed1f74e4a960c1665
Ola <hollabodey.kilaso@gmail.com>
2023-05-02 14:27:52 +0100

    Made chages to the html and css files

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let DarkModeBtn = document.getElementById('DarkMode');
+let DarkMode = document.getElementById('DarkMode');
 let container = document.querySelector('.container');
 
+let GreenMode = document.getElementById('green');
 
-DarkModeBtn.addEventListener('click', changeMode);
 
-function changeMode(e){
+DarkMode.addEventListener('click', darkMode);
+GreenMode.addEventListener('click', greenMode); 
+
+
+function darkMode(e){
     e.preventDefault();
     container.classList.toggle('darkMode');
 
+}
+
+function greenMode(e){
+    e.preventDefault();
+    container.classList.toggle('green');
 }
