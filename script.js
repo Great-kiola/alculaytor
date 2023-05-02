c6ffd7f0a1560cbb62220bc99cd7e27deb0eff88
Ola <hollabodey.kilaso@gmail.com>
2023-05-02 18:05:22 +0100

    Fix: added javascript to make calculator function

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let DarkMode = document.getElementById('DarkMode');
 let container = document.querySelector('.container');
-
 let GreenMode = document.getElementById('green');
-
 let Normal = document.getElementById('normal');
 
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((items) => {
+    items.onclick = () => {
+        if (items.id === 'delete'){
+            display.innerText = '';
+        } else if (items.id === 'AC'){
+            let string = display.innerText.toString();
+            display.innerText = string.substring(0, string.length - 1);
+        } else if (display.innerText != '' && items.id === 'equation'){
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && items.id === 'equation'){
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else {
+            display.innerText += items.id;
+        }
+    }
+})
+
+
+// Modes
 
 DarkMode.addEventListener('click', darkMode);
 GreenMode.addEventListener('click', greenMode);
